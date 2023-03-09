4d83750dd09ccdc283261e7bcd6b8834938170a5
tzubindev <71303907+tzubindev@users.noreply.github.com>
2023-03-09 16:33:41 +0800

    update

--- naivebayes.py
+++ naivebayes.py
@@ -47,9 +47,8 @@ class NaiveBayes:
 
         data = [(sentence, processed_data[sentence]) for sentence in processed_data]
         random.shuffle(data)
-        random.shuffle(data)
-        # training_data = data[: math.ceil(len(data) * 0.2)]
-        training_data = data
+        training_data = data[: math.ceil(len(data) * 0.2)]
+        # training_data = data
 
         self.cl = NaiveBayesClassifier(training_data)
 
