d711404e0bad0756c40ae4d765b49559325cd680
tzubindev <71303907+tzubindev@users.noreply.github.com>
2023-03-14 10:43:59 +0800

    enhance code structure

--- text_processing.py
+++ text_processing.py
@@ -1,5 +1,6 @@
 import string
 import nltk
+import json
 from nltk.tokenize import WhitespaceTokenizer
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
@@ -7,38 +8,33 @@ from nltk.tokenize import word_tokenize
 
 wnl = WordNetLemmatizer()
 tags = dict()
+data = None
 validLem = ["noun", "verb", "adverb", "adjective"]
 validLem_code = ["n", "v", "r", "a"]
 isTag = True
 pair_key = None
 
 # preload tags
-with open("./data/tags.json", "r") as f:
-    for line in f:
-        line = line.strip()
-        if line[0] not in ["[", "]", "{", "}"]:
-            data = " ".join(line.split('"tag":')).replace('"', "")
+with open("./data/tags.json") as f:
+    data = json.loads(f.read())["tags"]
 
-            if isTag:
-                data = " ".join(line.split('"tag":')).replace('"', "").strip()
-                pair_key = data[:-1]
-            else:
-                data = (
-                    " ".join(line.split('"meaning":'))
-                    .replace('"', "")
-                    .strip()
-                    .replace(",", "")
-                )
-                tags[pair_key] = data
-            isTag = not isTag
+for tag in data:
+    tags[tag["tag"]] = "".join([c for c in tag["meaning"] if c != ","]).lower()
 
 
 class TextProcessing:
     def __init__(self, sentence) -> None:
         nltk.download("stopwords")
-        stop_words = set(stopwords.words("english"))
+        self.stop_words = set(stopwords.words("english"))
         word_tokens = word_tokenize(sentence)
-        self.s = " ".join([w for w in word_tokens if w.lower() not in stop_words])
+        self.original_s = sentence
+        self.s = " ".join(
+            [
+                w.lower().strip("0123456789").strip(string.punctuation)
+                for w in word_tokens
+                if w.lower() not in self.stop_words
+            ]
+        )
 
     def getProcessedSentence(self):
         label = self.s
@@ -48,24 +44,13 @@ class TextProcessing:
             .replace("'s", "")
             .replace("'ve", " have")
         )
-        # print(label)
-        new_label = ""
-        for char in label:
-            if char in string.punctuation:
-                new_label += " " + char + " "
-            else:
-                new_label += char
 
-        words = nltk.pos_tag(WhitespaceTokenizer().tokenize(new_label))
+        words = nltk.pos_tag(WhitespaceTokenizer().tokenize(label))
         processed_sentence = ""
-        new_label = new_label.lower()  # remove CAPT
 
         for comb in words:
             # print(comb)
             word, tag = comb
-
-            word = word.strip("0123456789")  # remove NUM
-            word = word.strip(string.punctuation)  # remove PUNC
 
             tags_sep = tags[tag].split()
             target = None
@@ -78,16 +63,15 @@ class TextProcessing:
                     word = wnl.lemmatize(word.lower(), target)
             processed_sentence += f"{word} "
 
-        processed_sentence = " ".join(processed_sentence.strip().split())
-        return processed_sentence
+        return processed_sentence.strip()
 
     def getWordCounts(self):
         from textblob import TextBlob
 
-        blob = TextBlob(self.s)
+        blob = TextBlob(self.original_s)
+
         counts = dict()
-        for word in blob.words:
-            word = str(word).lower()
+        for word in [w for w in blob.words if w.lower() not in self.stop_words]:
             counts[word] = blob.word_counts[word]
 
         return counts
