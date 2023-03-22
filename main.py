dc10d8e3e23e614d2354a2317e711afa60a7a578
tzubindev <71303907+tzubindev@users.noreply.github.com>
2023-03-22 12:30:11 +0800

    update

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from transcribe import Transcribe
 from naivebayes import NaiveBayes
 from textblob import TextBlob
 import pyodbc as odcb
-import json 
+import json
 import collections
 from fastapi import Body
 from dotenv import dotenv_values
@@ -22,6 +22,8 @@ connection_string = f"DRIVER={{SQL Server}};SERVER={config['SERVER']};DATABASE={
 conn = odcb.connect(connection_string)
 
 cursor = conn.cursor()
+
+
 class AudioQuery(BaseModel):
     url: str
 
@@ -34,15 +36,19 @@ class CommentQuery(BaseModel):
     comment: str = Body(...)
     old_comment: str = Body(...)
 
+
 class SenderQuery(BaseModel):
     sender: str = Body(...)
     old_sender: str = Body(...)
 
+
 # class RequestUpdate(BaseModel):
 #     highest_Count: int = Body(...)
 
+
 class RequestDelete(BaseModel):
     Request_ID: int = Body(...)
+
 
 classifier = NaiveBayes()
 
@@ -126,9 +132,10 @@ def STT_Audio(query: AudioQuery):
 # def PostText(query: TextQuery):
 #     return {"response": query.string}
 
-@app.post("/stt/database")
-def Test(query: AudioQuery):
-    
+
+@app.post("/stt/addRequest")
+def AddRequest(query: AudioQuery):
+
     result = None
     sorted_word_counts = None
     MarkedWords = None
@@ -158,39 +165,46 @@ def Test(query: AudioQuery):
     sentiment = classifier.classifySentences(result)
 
     request_Record_list = []
-    request_Record_1 = ["", query.url, sentiment["Positive"], sentiment["Negative"], 3, "22-01-2023"]
+    request_Record_1 = [
+        "",
+        query.url,
+        sentiment["Positive"],
+        sentiment["Negative"],
+        3,
+        "22-01-2023",
+    ]
     request_Record_list.append(request_Record_1)
-    reuqest_insert_records = '''INSERT INTO Request 
+    reuqest_insert_records = """INSERT INTO Request 
                         (Request_ID, Audio_URL, Sentiment_Distribution_Pos, 
                         Sentiment_Distribution_Neg, highest_Count, Date) 
-                        VALUES (?,?,?,?,?,?)''' 
-    cursor.executemany(reuqest_insert_records, request_Record_list )
+                        VALUES (?,?,?,?,?,?)"""
+    cursor.executemany(reuqest_insert_records, request_Record_list)
     conn.commit()
     return {"response": query.url}
 
 
-    
 @app.delete("/stt/delete")
 async def DeleteData(request_delete: RequestDelete):
 
-        conn.autocommit = False
-        cursor = conn.cursor()
+    conn.autocommit = False
+    cursor = conn.cursor()
 
-        delete_query = """DELETE FROM Request WHERE Request_ID=?"""
-    
-        value = (request_delete.Request_ID)
+    delete_query = """DELETE FROM Request WHERE Request_ID=?"""
 
-        cursor.execute(delete_query, value)
+    value = request_delete.Request_ID
 
-        conn.commit()
+    cursor.execute(delete_query, value)
 
-        return {"message": "Data deleted successfully."}
+    conn.commit()
+
+    return {"message": "Data deleted successfully."}
+
 
 @app.post("/stt/updateComment/{conversation_id}/{request_id}")
 def updateComment(conversation_id: int, request_id: str, comment_update: CommentQuery):
     cursor = conn.cursor()
 
-    select_query = '''SELECT Comment FROM Conversations WHERE Request_ID= ? AND Conversation_ID=? '''
+    select_query = """SELECT Comment FROM Conversations WHERE Request_ID= ? AND Conversation_ID=? """
 
     values = (request_id, conversation_id)
 
@@ -200,9 +214,8 @@ def updateComment(conversation_id: int, request_id: str, comment_update: Comment
     if comment_update.old_comment != db_old_comment:
         conn.commit()
         return {"response": "Error"}
-    
 
-    update_query = '''UPDATE Conversations SET Comment = ? WHERE Request_ID= ? AND Conversation_ID=? '''
+    update_query = """UPDATE Conversations SET Comment = ? WHERE Request_ID= ? AND Conversation_ID=? """
 
     values = (comment_update.comment, request_id, conversation_id)
 
@@ -212,11 +225,12 @@ def updateComment(conversation_id: int, request_id: str, comment_update: Comment
 
     return {"response": "Success"}
 
+
 @app.post("/stt/updateSender/{conversation_id}/{request_id}")
 def updateSender(conversation_id: int, request_id: str, sender_update: SenderQuery):
     cursor = conn.cursor()
 
-    select_query = '''SELECT Sender FROM Conversations WHERE Request_ID= ? AND Conversation_ID=? '''
+    select_query = """SELECT Sender FROM Conversations WHERE Request_ID= ? AND Conversation_ID=? """
 
     values = (request_id, conversation_id)
 
@@ -226,8 +240,8 @@ def updateSender(conversation_id: int, request_id: str, sender_update: SenderQue
     if sender_update.old_sender != db_old_sender:
         conn.commit()
         return {"response": "Error"}
-    
-    update_query = '''UPDATE Conversations SET Sender = ? WHERE Request_ID= ? AND Conversation_ID=? '''
+
+    update_query = """UPDATE Conversations SET Sender = ? WHERE Request_ID= ? AND Conversation_ID=? """
 
     values = (sender_update.sender, request_id, conversation_id)
 
@@ -237,52 +251,73 @@ def updateSender(conversation_id: int, request_id: str, sender_update: SenderQue
 
     return {"response": "Updated Successfully"}
 
+
 @app.get("/stt/requests/{org_id}")
 async def ReturnRequests(org_id: str):
     cursor = conn.cursor()
-    
+
     cursor.execute("SELECT Request_ID FROM Request WHERE Org_ID = ? ", org_id)
     rows = cursor.fetchall()
     RequestList = []
     for row in rows:
-        data = (row[0])
+        data = row[0]
         RequestList.append(data)
 
     result = json.dumps(RequestList)
-   
+
     return {"Request": result}
+
 
 @app.get("/stt/request/{req_id}")
 async def STT_Test(req_id: str):
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM Request WHERE Request_ID = ? ", req_id)
     rows = cursor.fetchall()
-   
-    for row in rows:
-        data = {'request_id': row[0], 'sentiment': {'distribution':{'pos':row[2], 'neg':row[3]}}, 'highest_count': row[4], 'date': row[5]}
 
+    for row in rows:
+        data = {
+            "request_id": row[0],
+            "sentiment": {"distribution": {"pos": row[2], "neg": row[3]}},
+            "highest_count": row[4],
+            "date": row[5],
+        }
 
     cursor.execute("SELECT * FROM Words WHERE Request_ID = ? ", req_id)
     rows = cursor.fetchall()
     objects_list = []
     for row in rows:
-        object_dict = {'word_id': row[0], 'word': row[1], 'isClicked': False, 'isSearched': True, 'isSensitive': row[2], 'count': row[3]}
+        object_dict = {
+            "word_id": row[0],
+            "word": row[1],
+            "isClicked": False,
+            "isSearched": True,
+            "isSensitive": row[2],
+            "count": row[3],
+        }
         objects_list.append(object_dict)
 
     cursor.execute("SELECT * FROM Conversations WHERE Request_ID = ? ", req_id)
     rows = cursor.fetchall()
     objects_list1 = []
     for row in rows:
-        object_dict = {'conversation_id': row[0], 'from': row[1], 'content': row[2], 'sentiment': row[3], 'confidence': row[4], 'isClicked': False,'comment': row[5]}
+        object_dict = {
+            "conversation_id": row[0],
+            "from": row[1],
+            "content": row[2],
+            "sentiment": row[3],
+            "confidence": row[4],
+            "isClicked": False,
+            "comment": row[5],
+        }
         objects_list1.append(object_dict)
 
     result_dict = {
-    "request_id": data['request_id'],
-    "sentiment": data['sentiment'],
-    "highest_count": data['highest_count'],
-    "date": data['date'],
-    "words": objects_list,
-    "conversations": objects_list1
+        "request_id": data["request_id"],
+        "sentiment": data["sentiment"],
+        "highest_count": data["highest_count"],
+        "date": data["date"],
+        "words": objects_list,
+        "conversations": objects_list1,
     }
 
     result = json.dumps(result_dict)
@@ -291,6 +326,3 @@ async def STT_Test(req_id: str):
     #     outfile.write(result)
 
     return {"Request": result}
-
-
- 
